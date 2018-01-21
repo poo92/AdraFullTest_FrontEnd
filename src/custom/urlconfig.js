9f013a239f3b5bdedf74ee7a63d9f8bf21a6649b
poo92 <karunarathnepoornima@gmail.com>
2018-01-21 23:56:10 +0530

    https added

--- src/custom/urlconfig.js
+++ src/custom/urlconfig.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:25882/";
-export const BASE_URL = "http://adrafulltest.azurewebsites.net/";
+export const BASE_URL = "https://adrafulltest.azurewebsites.net/";
 
